de5603ff2e7f51318e182575703d006d7519e7c4
Maria <marlundh25@gmail.com>
2020-05-11 12:36:28 +0300

    Add forEach functions.

--- learnenough_tutorial/js_tutorial/functions.js
+++ learnenough_tutorial/js_tutorial/functions.js
@@ -16,3 +16,42 @@ function emailParts(email){
   let ignoreCase = email.toLowerCase()
   return ignoreCase.split("@");
 }
+
+/*
+  // Usage of for loop
+for (let i = 0; i array.length; i++){
+  console.log(array[i]);
+};
+
+  // Usage of forEach loop
+array.forEach(function(element) {
+  console.log(element);
+  });
+*/
+
+  // Examples of forEach loops
+let a=["ant", "bat", "cat", 42];
+a.forEach(function(element){
+  console.log(element);
+});
+
+let soliloquy = "To be, or not to be, that is the question:";
+Array.from(soliloquy).forEach(function(character){
+  console.log(character);
+});
+
+let strings = "Honey Badger";
+Array.from(strings).forEach((strings) => {
+  console.log(strings);
+});
+
+let numberSorting = [8, 17, 42, 99];
+numberSorting.sort(function(a, b) { return b - a;});
+
+numberSorting.forEach(function(single){
+  console.log(single);
+});
+
+numberSorting.forEach((number) =>{
+  console.log(number);
+});
